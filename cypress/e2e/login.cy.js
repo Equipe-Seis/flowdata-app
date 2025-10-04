6f7c6f7ce96774a22b45faef106b104130e0883d
devmdi <devweb.dic@gmail.com>
2025-10-04 14:14:10 -0300

    testes atualizados

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,7 +7,7 @@ describe('Tela de Login - FlowData', () => {
   })
 
    it('Deve permitir realizar o login com credenciais válidas', () => {  
-        cy.get('#login-email-input').type('admin@teste.com');
+        cy.get('#login-email-input',{timeout:10000}).type('admin@flowdata.com');
         cy.get('#login-pass-input').type('2025@flow');
         cy.get('#login-save-button').click(); 
 })
--- cypress/e2e/recebimentos.cy.js
+++ cypress/e2e/recebimentos.cy.js
@@ -1,12 +1,42 @@
-describe('Recebimento ', () => {
+/// <reference types="cypress" />
 
+describe('Recebimento - FlowData', () => {
+  
   beforeEach(() => {
-    cy.visit('/http://localhost:3001/stock/checking')
+    cy.visit('http://localhost:3001/login')
   })
 
-  it('Cenário 1: Concluir recebimento com sucesso', () => {
-    cy.get('#input-v-0-12').first().click()
+   it('Deve permitir realizar o login com credenciais válidas', () => {  
+        cy.get('#login-email-input',{timeout:10000}).type('admin@flowdata.com');
+        cy.get('#login-pass-input').type('2025@flow');
+        cy.get('#login-save-button').click(); 
+})
 
+it(' Deve cadastrar e concluir recebimento com sucesso ', () => {  
+  cy.get('#login-email-input',{timeout:10000}).type('admin@flowdata.com');
+  cy.get('#login-pass-input').type('2025@flow');
+  cy.get('#login-save-button').click(); 
+  cy.get('.mdi-menu').click();
+  cy.get('#v-list-group--id-Estoque').click();
+  cy.get('[href="/stock/checking"]').click();
+  cy.get('#stock-create-button').click();
+  cy.get('#stock-codpro-input').click().type('SUP-0002');
+  cy.get('#stock-prod-confirm-button').click();
+  cy.get('#stock-received-qty-input').click().type(80);
+  cy.get('#stock-crete-line-button > .v-btn__content').click();
+})
 
-
+it(' Deve gerar um erro ao ter um recebimento vazio ', () => {  
+  cy.get('#login-email-input',{timeout:10000}).type('admin@flowdata.com');
+  cy.get('#login-pass-input').type('2025@flow');
+  cy.get('#login-save-button').click(); 
+  cy.get('.mdi-menu').click();
+  cy.get('#v-list-group--id-Estoque').click();
+  cy.get('[href="/stock/checking"]').click();
+  cy.get('#stock-create-button').click();
+  cy.get('#stock-codpro-input').click().type('');
+  cy.get('#stock-prod-confirm-button').click();
+  cy.get('#stock-received-qty-input').click().type(80);
+  cy.get('#stock-crete-line-button > .v-btn__content').click();
+})
 })
