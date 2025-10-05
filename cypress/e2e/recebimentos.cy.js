e0a95dffe5ad96339739f9f76244ae312e23cfaf
danibenfica <daniele.benfica0@gmail.com>
2025-10-04 21:26:12 -0300

    adicionando testes

--- cypress/e2e/recebimentos.cy.js
+++ cypress/e2e/recebimentos.cy.js
@@ -6,12 +6,6 @@ describe('Recebimento - FlowData', () => {
     cy.visit('http://localhost:3001/login')
   })
 
-   it('Deve permitir realizar o login com credenciais válidas', () => {  
-        cy.get('#login-email-input',{timeout:10000}).type('admin@flowdata.com');
-        cy.get('#login-pass-input').type('2025@flow');
-        cy.get('#login-save-button').click(); 
-})
-
     it(' Deve cadastrar e concluir recebimento com sucesso ', () => {  
         cy.get('#login-email-input',{timeout:10000}).type('admin@flowdata.com');
         cy.get('#login-pass-input').type('2025@flow');
@@ -20,10 +14,13 @@ describe('Recebimento - FlowData', () => {
         cy.get('#v-list-group--id-Estoque').click();
         cy.get('[href="/stock/checking"]').click();
         cy.get('#stock-create-button').click();
-        cy.get('#stock-codpro-input').click().type('SUP-0002');
+        cy.get('#stock-codpro-input').click().type('123456');
         cy.get('#stock-prod-confirm-button').click();
-        cy.get('#stock-received-qty-input').click().type(80);
+        cy.get('#stock-received-qty-input').click().type(22);
         cy.get('#stock-crete-line-button > .v-btn__content').click();
+        cy.get('#stock-end-button').click();
+        cy.get('#stock-conclude-checking-button').click();
+        cy.get('.v-card-item').should('contain', 'Recebimento concluído');
       })
 
   it(' Deve gerar um erro ao ter um recebimento vazio ', () => {  
